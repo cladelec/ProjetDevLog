c4cc52599c7b0894a4238a00e5d72da7052f11b3
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2018-04-30 17:12:53 +0200

    Classe mére bactérie

--- Bacterie.h
+++ Bacterie.h
@@ -3,7 +3,23 @@
 
 
 class Bacterie { 
-
+	public : 
+		//Constructor
+		Bacterie(); 
+		//Destructor 
+		virtual ~Bacterie();
+		//Methods
+		virtual void metaboliser()=0;
+		
+	protected : 
+		float Pdeath; 
+		float Pmut; 
+		float Wmin; 
+		float w_;
+		
+		float A_;
+		float B_; 
+		float C_;
 
 };
 #endif 
