09f3adf84cbcc6589f5e9e2cf514f18227a8af57
marie bp <marie.bourdet-pees@insa-lyon.fr>
2018-05-16 20:04:05 +0200

    verif destructeur Bactérie

--- main.cpp
+++ main.cpp
@@ -13,14 +13,16 @@
 int main () {
 	printf("Hello world \n");
 	
-	Grille* laGrille = new Grille(32,32,20.0,0.0,0.02,0.1,0.1,0.1,0.1,0.1);
+	Grille* laGrille = new Grille(32,32,20.0,0.0,0.8,0.1,0.1,0.1,0.1,0.1);
 	cout<<laGrille->to_string()<<endl;
 	cout<<laGrille->affichage()<<endl;
 	
-	//laGrille.run();
-	//cout<<laGrille->to_string()<<endl;
+	laGrille->run();
+	cout<<laGrille->to_string()<<endl;
 
-	delete laGrille; //destructeur gère pas ouf nb instances si 2 grilles....
+
+	delete laGrille;
+
 
 	Grille laPititeGrille(4,4,2,0.0,0.02,0.1,0.1,0.1,0.1,0.1);
 	cout<<laPititeGrille.to_string()<<endl;
