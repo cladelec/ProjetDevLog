6409e5712507b864c7bb0707bf9a00a26261add7
Clara Delecroix <clara.delecroix@insa-lyon.fr>
2018-05-18 09:00:00 +0200

    test dans le main

--- main.cpp
+++ main.cpp
@@ -13,11 +13,13 @@
 int main () {
 	printf("Hello world \n");
 	
-	Grille* laGrille = new Grille(32,32,20.0,0.0,1.0,0.1,0.1,0.1,0.1,0.1);
+	Grille* laGrille = new Grille(32,32,20.0,0.0,0.02,0.1,0.1,0.1,0.1,0.1);
 	cout<<laGrille->to_string()<<endl;
 	cout<<laGrille->affichage()<<endl;
 	
-	laGrille->run();
+	for(int i=0;i<5;++i){
+		laGrille->run();
+	}
 	cout<<laGrille->to_string()<<endl;
 	cout<<laGrille->affichage()<<endl;
 
