28283572abb885be8c02da5cac92d1892a9ef6d3
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2018-05-18 09:01:20 +0200

    Modif main

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 // ===========================================================================
 int main () {
 	printf("Hello world \n");
-	
+	/*
 	Grille* laGrille = new Grille(32,32,20.0,0.0,1.0,0.1,0.1,0.1,0.1,0.1);
 	cout<<laGrille->to_string()<<endl;
 	cout<<laGrille->affichage()<<endl;
@@ -22,15 +22,22 @@ int main () {
 	cout<<laGrille->affichage()<<endl;
 
 	delete laGrille;
-
+*/
 
 	Grille* laPititeGrille = new Grille(3,3,2,0.0,0.02,0.1,0.1,0.1,0.1,0.1);
+	/*
 	cout<<laPititeGrille->to_string()<<endl;
 	cout<<laPititeGrille->affichage()<<endl; 
 	float proba=(rand()%100+1); 
 	std::cout << proba << std::endl; 
-
+	*/
+	int i;
+	for(i=0;i<100;++i) { 
+		laPititeGrille->run();
+		cout<<laPititeGrille->to_string()<<endl;
+		cout<<laPititeGrille->affichage()<<endl;
+	}
 	
-	
+	delete laPititeGrille;
 	return 0;
 }
